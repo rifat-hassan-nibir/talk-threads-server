b91a76174f6ac98f32eff014be7cdc335fe3dcdf
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-03 10:39:02 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Talk threads server running");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
