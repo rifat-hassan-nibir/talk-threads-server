5a0754396fa1a007588969d977ced079e747ceb5
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-03 12:40:45 +0600

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Talk threads server running fine");
+  res.send("Talk threads server running perfectly");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
