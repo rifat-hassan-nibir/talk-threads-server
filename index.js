e8a9a79a6deae5bf11f09dd97fca8a5baf099c35
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-08 09:24:52 +0600

    delete post api created

--- index.js
+++ index.js
@@ -63,6 +63,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a post using id
+    app.delete("/delete-post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
