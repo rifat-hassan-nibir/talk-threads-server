1390d8b98beefd994e456a977d8eb48bafc182e3
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-16 11:21:15 +0600

    added vercel.json file and added firebase hostin email to corsOptions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://talk-threads.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
